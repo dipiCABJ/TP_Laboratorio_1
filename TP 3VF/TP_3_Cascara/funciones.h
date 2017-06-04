6b792b0a13371339e93c283d4d76188aa4334b9b
@dipiCABJ <chichocat@gmail.com>
2017-06-04 00:54:07 -0300

    Add files via upload
    Corrección de error al cargar la imagen en el HTML, y agregado de un archivo Peliculas.txt para acelerar la carga de datos durante la evaluación

---
+++ TP 3VF/TP_3_Cascara/funciones.h
@@ -0,0 +1,127 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+typedef struct{
+    char titulo[50];
+    char genero[20];
+    int duracion;
+    char descripcion[500];
+    int puntaje;
+    char linkImagen[200];
+    int activeStatus;
+}EMovie;
+
+/** \brief Inicializa el Vector tipo estructura en estado en -1
+ *
+ * \param vector de moviesmovie->linkImagen
+ * \param cantidad de elementos
+ * \param valor de inicializacion
+ * \return no devuelve valor
+ *
+ */
+void inicializarActiveStatus(EMovie movie[],int cant_elem,int valor);
+/**
+ *  Agrega una pelicula al archivo binario
+ *  @param movie la estructura a ser agregada al archivo
+ *  @return retorna 1 o 0 de acuerdo a si pudo agregar la pelicula o no
+ */
+int agregarPelicula(EMovie*,int*);
+
+/**
+ *  Borra una pelicula del archivo binario
+ *  @param movie la estructura a ser eliminada al archivo
+ *  @return retorna 1 o 0 de acuerdo a si pudo eliminar la pelicula o no
+ */
+int borrarPelicula(EMovie*,int);
+
+/**
+ *  Modifica una pelicula del archivo binario
+ *  @param movie la estructura a ser eliminada al archivo
+ *  @return retorna 1 o 0 de acuerdo a si pudo modificar la pelicula o no
+ */
+int modificarPelicula(EMovie* movie,int);
+
+/**
+ *  Genera un archivo html a partir de las peliculas cargadas en el archivo binario.
+ *  @param lista la lista de peliculas a ser agregadas en el archivo.
+ *  @param nombre el nombre para el archivo.
+ */
+void generarPagina(EMovie lista[], char nombre[],int);
+
+/**
+ *  Genera espacio en memoria para el array de tipo EMovie
+ *  @param array de estructuras de tipo EMovie
+ *  @param cantidad de elementos del array de tipo EMovie
+ *  @return Puntero al comienzo de la posicion de memoria de ese array asignado
+ */
+EMovie* newMovies();
+
+/** \brief Obtiene un vector de solo numeros, llama a la funcion "IsNumeric"
+ *
+ * \param Mensaje que sera mostrado al usuario
+ * \param vector donde se cargara el ingreso
+ * \return luego de la validacion numerica, devuelve un 1 si fue correcta la carga o 0 si no paso la validacion numerica
+ *
+ */
+int obtenerNumeros(char[],char[]);
+
+/** \brief Valida el ingreso de solo numeros
+ *
+ * \param recibe vector de tipo char a validar
+ * \return devuelve 1 si la validacion es correcta, 0 si no lo es
+ *
+ */
+int isNumeric(char[]);
+
+/** \brief Obtiene un vector de solo letras, llama a la funcion "IsString"
+ *
+ * \param Mensaje que sera mostrado al usuario
+ * \param vector donde se cargara el ingreso
+ * \return luego de la validacion caracter a caracter, devuelve un 1 si fue correcta la carga o 0 si no paso la validacion
+ *
+ */
+int obtenerCadena(char[],char[]);
+
+/** \brief Valida el ingreso de solo letras
+ *
+ * \param recibe vector de tipo char a validar
+ * \return devuelve 1 si la validacion es correcta, 0 si no lo es
+ *
+ */
+int isString(char[]);
+
+/**
+ * \brief Busca un idx libre para la carga de datos
+ * \param vector como puntero a cargar
+ * \param cantidad de elementos a recorrer
+ * \param vector a buscar en activeStatus
+ * \return El indice libre para la carga
+ *
+ */
+int buscarIdxLibre(EMovie*,int,int);
+
+/** \brief Pide un entero al usuario
+ *
+ * \param Mensaje que sera mostrado al usuario
+ * \return devuelve el entero cargado
+ *
+ */
+int getInt(char []);
+
+/**
+ * \brief Verifica si el valor recibido contiene solo letras, números y puede tener espacios
+ * \param str Array con la cadena a ser analizada
+ * \return 1 si contiene solo espacio o letras y números, y 0 si no lo es
+ *
+ */
+int esAlfaNumerico(char[]);
+
+/**
+ *  Genera un realloc de espacio en memoria para el array de tipo EMovie
+ *  @param array de estructuras de tipo EMovie
+ *  @param cantidad de elementos del array de tipo EMovie
+ *  @return Puntero al comienzo de la posicion de memoria de ese array asignado
+ */
+EMovie* newVectorMovies(EMovie*,int);
+
+#endif // FUNCIONES_H_INCLUDED
---
+++ TP 3VF/TP_3_Cascara/main.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "funciones.h"
+//#define tam 6
+
+int main()
+{
+    system("color 1e");
+    EMovie* movies = newMovies();
+    EMovie* auxiliar = NULL;
+    FILE* pFileMovie;
+    char seguir='s';
+    int opcion=0,CantMovies=0,i;//largo=0;//,contador=0;//lec,cont=0;
+    //inicializarActiveStatus(movies,tam,-1);
+    while(seguir=='s')
+    {
+        printf("1- Agregar pelicula\n");
+        printf("2- Borrar pelicula\n");
+        printf("3- Modificar pelicula\n");
+        printf("4- Generar pagina web\n");
+        printf("5- Salir\n");
+        printf("Elija una opcion: ");
+
+        scanf("%d",&opcion);
+        while(opcion < 1 || opcion > 5)
+        {
+           printf("Opcio incorrecta, reingrese!!: ");
+           scanf("%d",&opcion);
+        }
+        //validar opcion incorrecta!!
+        switch(opcion)
+        {
+            case 1:
+                system("cls");
+                if(!(agregarPelicula(movies,&CantMovies)))
+                {
+                  printf("No se pudo dar de alta la Pelicula!!");
+                  break;
+                }
+                auxiliar = (EMovie*)realloc(movies,sizeof(EMovie)*(CantMovies+1));
+                if(auxiliar == NULL)
+                {
+                   printf("No se pudo reasignar memoria!!\n");
+                   exit(1);
+                }
+                movies = auxiliar;
+                system("pause");
+                system("cls");
+                break;
+            case 2:
+                if(!(borrarPelicula(movies,CantMovies)))
+                {
+                  printf("La Pelicula no pudo ser dada de baja!!");
+                  break;
+                }
+                system("pause");
+                printf("\n\n");
+                for(i=0;i<CantMovies;i++)
+                {
+                  if((movies + i)->activeStatus != 1)
+                    continue;
+                  printf("Titulo: %s\n",(movies + i)->titulo);
+                }
+                system("pause");
+                system("cls");
+                break;
+            case 3:
+                /*pFileMovie = fopen("movies.bin","rb");
+                if(pFileMovie == NULL)
+                {
+                   printf("No se puede abrir el Archivo!!");
+                   exit(1);
+                }
+                while(fread((movies+largo),sizeof(EMovie),1,pFileMovie)!=0)
+                 {
+                      printf("Titulo: %s\t Estado: %d\n",(movies + largo)->titulo,(movies + largo)->activeStatus);
+                      largo++;
+                      auxiliar = (EMovie*)realloc(movies,sizeof(EMovie)*(largo));
+                      if(auxiliar == NULL)
+                      {
+                         printf("No se puede asignar memoria!!");
+                         LEE 2 PELICULAS DEL ARCHIVO BINARIO PARA PRESENTAR AL USUARIO
+                         Y ENTRA ACA (?)
+                         exit(1);
+                      }
+                      //movies = auxiliar;
+                 }
+                 movies = auxiliar;
+                 fclose(pFileMovie);
+                 free(auxiliar);*/
+                 for(i=0;i<CantMovies;i++)
+                 {
+                    if((movies + i)->activeStatus == -1)
+                        continue;
+                    printf("Titulo: %s\tEstado: %d\n",(movies + i)->titulo,(movies + i)->activeStatus);
+                 }
+                if(!(modificarPelicula(movies,CantMovies)))
+                {
+                  printf("La Pelicula no pudo ser modificada!!");
+                  break;
+                }
+                for(i=0;i<CantMovies;i++)
+                {
+                  if((movies + i)->activeStatus == 1)
+                  //fwrite((peli + i),sizeof(EMovie),1,arch);
+
+                  printf("Titulo: %s\n",(movies + i)->titulo);
+                }
+                system("pause");
+                system("cls");
+                break;
+            case 4:
+                if(CantMovies == 0)
+                {
+                   printf("No hay peliculas cargadas, no se generara el archivo!!\n");
+                   system("pause");
+                   system("cls");
+                   break;
+                }
+                generarPagina(movies,"Index.html",CantMovies);
+                free(movies);
+                break;
+            case 5:
+                if(CantMovies == 0)
+                {
+                   printf("No hay peliculas cargadas, no se generara el archivo!!\n");
+                   system("pause");
+                   system("cls");
+                   break;
+                }
+                printf("Se generara el archivo binario para resguardar la informacion\n\n");
+                pFileMovie = fopen("movies.bin","ab+");
+                if(pFileMovie==NULL)
+                 {
+                   printf("Error abriendo archivo!!");
+                   exit(1);
+                 }
+                for(i=0;i<CantMovies;i++)
+                 {
+                  if((movies + i)->activeStatus == 1)
+                  fwrite((movies + i),sizeof(EMovie),1,pFileMovie);
+                  //printf("Titulo: %s\n",(movies + i)->titulo);
+                 }
+                fclose(pFileMovie);
+                free(movies);
+                system("pause");
+                system("cls");
+                seguir = 'n';
+                break;
+        }
+    }
+
+    return 0;
+}
